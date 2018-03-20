f276cb01aaf5036e52f41dd6a3e0270064f42176
esterTion <lcz970@126.com>
2018-03-20 22:16:33 +0800

    180320

--- live_html5_pack.js
+++ live_html5_pack.js
@@ -1012,18 +1012,10 @@ var CommentManager = (function() {
 		this.paused = true;
 		this.pausedTime = 0;
 		this.canvas = document.createElement('canvas');
-		this.canvasStatic = document.createElement('canvas');
-		this.staticUpdate = false;
 		this.canvasFPS = 0;
 		self.canvas.style.width = '100%';
 		self.canvas.style.height = '100%';
 		self.stage.appendChild(self.canvas);
-		self.canvasStatic.style.width = '100%';
-		self.canvasStatic.style.height = '100%';
-		self.canvasStatic.style.position='absolute';
-		self.canvasStatic.style.top=0;
-		self.canvasStatic.style.left=0;
-		self.stage.appendChild(self.canvasStatic);
 		this.options = {
 			global:{
 				opacity:1,
@@ -1031,7 +1023,10 @@ var CommentManager = (function() {
 				className:"cmt",
 				useCSS:false,
 				autoOpacity:false,
-				autoOpacityVal:1
+				autoOpacityVal:1,
+				density: 0,
+				outline: false,
+				shadow: true
 			},
 			scroll:{
 				opacity:1,
@@ -1118,12 +1113,13 @@ var CommentManager = (function() {
 		}
 		this.sendQueueLoader = function(){
 			var start = performance.now(),passed;
-			while(sendQueue.length > 0){
+			sendQueue.length && self.sendAsync(sendQueue.shift());
+			/*while(sendQueue.length > 0){
 				self.sendAsync(sendQueue.shift());
 				passed = performance.now()-start;
-				if(passed > 8)
+				if(passed > 5)
 					return;
-			}
+			}*/
 		}
 		this.addEventListener('clear',function(){
 			sendQueue=[];
@@ -1137,7 +1133,7 @@ var CommentManager = (function() {
 				if(prevRatio != ratio){
 					self.runline.forEach(function(i){
 						if(i.textData)
-						commentCanvasDrawer(i)
+						commentCanvasDrawer(i, self.options.global.outline, self.options.global.shadow)
 					})
 					prevRatio = ratio;
 				}
@@ -1220,7 +1216,6 @@ var CommentManager = (function() {
 		}
 		this.dispatchEvent("clear");
 		this.canvas.getContext('2d').clearRect(0,0,this.canvas.width,this.canvas.height);
-		this.canvasStatic.getContext('2d').clearRect(0,0,this.canvasStatic.width,this.canvasStatic.height);
 	};
 
 	CommentManager.prototype.setBounds = function(){
@@ -1233,6 +1228,7 @@ var CommentManager = (function() {
 		// Update 3d perspective
 		this.stage.style.perspective = this.width * Math.tan(40 * Math.PI/180) / 2 + "px";
 		this.stage.style.webkitPerspective = this.width * Math.tan(40 * Math.PI/180) / 2 + "px";
+		this.canvasResize();
 	};
 	CommentManager.prototype.init = function(){
 		this.setBounds();
@@ -1267,10 +1263,9 @@ var CommentManager = (function() {
 		try{
 		var w=this.width,h=this.height,devicePixelRatio=window.devicePixelRatio;
 		this.canvas.width = this.canvas.offsetWidth * devicePixelRatio;
-		this.canvasStatic.height = this.canvasStatic.offsetHeight * devicePixelRatio;
+		this.canvas.height = this.canvas.offsetHeight * devicePixelRatio;
 		this.ttlRecalcAll();
-		canvasDrawStatic(this);
-		canvasDrawScroll(this);
+		canvasDraw(this);
 		
 		}catch(e){
 			console.error('shit happened! forcing CSS! ',e.message);
@@ -1278,88 +1273,13 @@ var CommentManager = (function() {
 			return;
 		}
 	};
-	var canvasDrawStatic=function(cmMgr){
-		var canvas=cmMgr.canvasStatic, ctx=canvas.getContext('2d'), devicePixelRatio = window.devicePixelRatio,
-		canvasWidth, canvasHeight = cmMgr.height,
-		x, y,
-		maxWidth=[0],
-		halfLeft;
-		cmMgr.runline.forEach(function(cmt){
-			if([4,5].indexOf(cmt.mode)!=-1){
-				maxWidth.push(cmt._width);
-			}
-		});
-		maxWidth=Math.min(cmMgr.width,Math.max.apply(Math,maxWidth));
-		canvasWidth = maxWidth;
-		halfLeft=(cmMgr.width-maxWidth)/2;
-		
-		if(maxWidth!=canvas.offsetWidth){
-			canvas.style.width=maxWidth+'px';
-			canvas.style.left='calc(50% - '+maxWidth/2+'px)'
-			canvas.width=maxWidth * devicePixelRatio;
-		}else{
-			ctx.clearRect(0, 0, canvasWidth * devicePixelRatio, canvasHeight * devicePixelRatio);
-		}
-		
-		ctx.globalAlpha=cmMgr.options.global.autoOpacity ? cmMgr.options.global.autoOpacityVal : cmMgr.options.global.opacity;
-		cmMgr.runline.forEach(function(cmt){
-			if(!cmt.textData)return;
-			switch(cmt.mode){
-				case 4:
-					//bottom
-					cmt.x = (canvasWidth - cmt.width) / 2+halfLeft;
-					x = cmt.x-halfLeft;
-					y = (canvasHeight - cmt.y - cmt.height);
-				break;
-				case 5:
-					//top
-					cmt.x = (canvasWidth - cmt.width) / 2+halfLeft;
-					x = cmt.x-halfLeft;
-					y = (cmt.y);
-				break;
-				default:
-					return;
-			}
-			ctx.drawImage(cmt.textData, round(x * devicePixelRatio), round(y * devicePixelRatio));
-		});
-	},
-	canvasDrawScroll=function(cmMgr){
+	var cachedMaxBottomLine=0,
+	cachedFixedBottomMaxWidth=0,
+	cachedFixedBottomHeight=0,
+	cachedFixedTopMaxWidth=0,
+	cachedFixedTopHeight=0,
+	canvasDraw=function(cmMgr){
 		//console.log('static call',performance.now())
-		var canvas=cmMgr.canvas, ctx=canvas.getContext('2d'), devicePixelRatio = window.devicePixelRatio,
-		canvasWidth = cmMgr.width, canvasHeight,
-		x, y,
-		drawCount=0,
-		maxBottomLine=[0];
-		cmMgr.runline.forEach(function(cmt){
-			if(cmt.mode==1){
-				maxBottomLine.push(cmt._y+cmt._height);
-			}
-		});
-		maxBottomLine=Math.max.apply(Math,maxBottomLine);
-		canvasHeight=maxBottomLine;
-		
-		if(canvas.offsetHeight!=maxBottomLine){
-			canvas.style.height=maxBottomLine+'px';
-			canvas.height=maxBottomLine * devicePixelRatio;
-		}else{
-			ctx.clearRect(0, 0, canvasWidth * devicePixelRatio, canvasHeight * devicePixelRatio);
-		}
-		
-		ctx.globalAlpha=cmMgr.options.global.autoOpacity ? cmMgr.options.global.autoOpacityVal : cmMgr.options.global.opacity;
-		cmMgr.runline.forEach(function(cmt){
-			if(!cmt.textData)return;
-			switch(cmt.mode){
-				case 1:
-					//scroll
-					cmt.x = canvasWidth - cmt.rx;
-					x = (canvasWidth - cmt.rx);
-					y = cmt.y;
-				break;
-				default:
-				return;
-			}
-			ctx.drawImage(cmt.textData, round(x * devicePixelRatio), round(y * devicePixelRatio));
-		});
 	}
 	CommentManager.prototype.canvasDrawer = function(){
 		if(this.options.global.useCSS){
@@ -1373,7 +1293,7 @@ var CommentManager = (function() {
 		ctx = this.canvas.getContext('2d'),
 		canvasWidth = this.width, canvasHeight = this.height,
 		x, y;
-		ctx.clearRect(0, 0, canvasWidth * devicePixelRatio, canvasHeight * devicePixelRatio);
+		//ctx.clearRect(0, 0, canvasWidth * devicePixelRatio, canvasHeight * devicePixelRatio);
 		ctx.globalAlpha=this.options.global.autoOpacity ? this.options.global.autoOpacityVal : this.options.global.opacity;
 		ctx.imageSmoothingEnabled = false;
 		
@@ -1393,23 +1313,97 @@ var CommentManager = (function() {
 			this.pausedTime = 0;
 		}
 		
+		var devicePixelRatio = window.devicePixelRatio,
+		maxBottomLine=[0];
+		maxBottomMaxWidth=[0],
+		maxBottomHeight=[0],
+		maxTopMaxWidth=[0],
+		maxTopHeight=[0];
 		this.runline.forEach(function(cmt){
+			if(cmt.mode==1){
+				cmt.rx += cmt.speed * ( now - cmt.prev ) / 1e3;
+				cmt.prev = now;
+				maxBottomLine.push(cmt._y+cmt._height);
+			} else if(cmt.mode == 4){
+				maxBottomMaxWidth.push(cmt._width);
+				maxBottomHeight.push(cmt._y+cmt._height);
+			} else if(cmt.mode == 5) {
+				maxTopMaxWidth.push(cmt._width);
+				maxTopHeight.push(cmt._y+cmt._height);
+			}
+		});
+		maxBottomLine=Math.max.apply(Math,maxBottomLine);
+		maxBottomMaxWidth=Math.max.apply(Math,maxBottomMaxWidth);
+		maxBottomHeight=Math.max.apply(Math,maxBottomHeight);
+		maxTopMaxWidth=Math.max.apply(Math,maxTopMaxWidth);
+		maxTopHeight=Math.max.apply(Math,maxTopHeight);
+		
+		//canvasDraw(this);
+		/**
+		 * 180303
+		 * canvas重构
+		 * 恢复全屏canvas
+		 * 记录顶部高度进行部分擦除
+		 * 0,0 到 width,滚动高
+		 * 顶宽左,滚动高 到 顶宽右,顶高 （范围内不擦除）
+		 * 底宽左,高-底高 到 底宽右,高 （范围按滚动高改动）
+		 */
+		
+		if(cachedMaxBottomLine > 0){
+			ctx.clearRect(0, 0, round(canvasWidth * devicePixelRatio), round(cachedMaxBottomLine * devicePixelRatio));
+		}
+		if(cachedFixedTopHeight>cachedMaxBottomLine) {
+			ctx.clearRect(
+				round((canvasWidth-cachedFixedTopMaxWidth)/2 * devicePixelRatio),
+				round(cachedMaxBottomLine * devicePixelRatio),
+				round((canvasWidth+cachedFixedTopMaxWidth)/2 * devicePixelRatio),
+				round(cachedFixedTopHeight * devicePixelRatio)
+			);
+		}
+		ctx.clearRect(
+			round((canvasWidth-cachedFixedBottomMaxWidth)/2 * devicePixelRatio),
+			round((Math.max(canvasHeight-cachedFixedBottomHeight, cachedMaxBottomLine)) * devicePixelRatio),
+			round((canvasWidth+cachedFixedBottomMaxWidth)/2 * devicePixelRatio),
+			round(canvasHeight * devicePixelRatio)
+		);
+		cachedMaxBottomLine=maxBottomLine;
+		cachedFixedBottomMaxWidth=maxBottomMaxWidth;
+		cachedFixedBottomHeight=maxBottomHeight;
+		cachedFixedTopMaxWidth=maxTopMaxWidth;
+		cachedFixedTopHeight=maxTopHeight;
+
+		//ctx.globalAlpha = 1;
+		//ctx.drawImage(window.abpinst.video, 0, 0);
+
+		var cmMgr=this;
+				
+		ctx.globalAlpha=cmMgr.options.global.autoOpacity ? cmMgr.options.global.autoOpacityVal : cmMgr.options.global.opacity;
+		cmMgr.runline.forEach(function(cmt){
 			if(!cmt.textData)return;
 			switch(cmt.mode){
 				case 1:
 					//scroll
-					cmt.rx += cmt.speed * ( now - cmt.prev ) / 1e3;
-					cmt.prev = now;
+					cmt.x = canvasWidth - cmt.rx;
+					x = (canvasWidth - cmt.rx);
+					y = cmt.y;
 				break;
+				case 4:
+					//bottom
+					cmt.x = (canvasWidth - cmt.width) / 2;
+					x = cmt.x;
+					y = (canvasHeight - cmt.y - cmt.height);
+				break;
+				case 5:
+					//top
+					cmt.x = (canvasWidth - cmt.width) / 2;
+					x = cmt.x;
+					y = (cmt.y);
+				break;
+				default:
+				return;
 			}
+			ctx.drawImage(cmt.textData, round(x * devicePixelRatio), round(y * devicePixelRatio));
 		});
-		
-		if(this.staticUpdate){
-			canvasDrawStatic(this);
-			this.staticUpdate=false;
-		}
-		
-		canvasDrawScroll(this);
 		//this.canvas.style.opacity = this.options.global.opacity;
 		//this.canvas.getContext('2d').putImageData(ctx.getImageData(0, 0, this.canvas.width, this.canvas.height), 0, 0)
 		requestAnimationFrame(this.canvasDrawerWrapper);
@@ -1430,9 +1424,11 @@ var CommentManager = (function() {
 			cmt.dur = ( cmt.parent.width + cmt.width ) / cmt.speed * 1e3;
 			cmt.ttl = cmt.dur - runned;
 		}
-	},commentCanvasDrawer = function(cmt){
+	},commentCanvasDrawer = function(cmt, outline, shadow){
 		var commentCanvas = document.createElement('canvas'), commentCanvasCtx = commentCanvas.getContext('2d'), devicePixelRatio = window.devicePixelRatio;
-		commentCanvasCtx.font = (cmt.size * devicePixelRatio) + 'px ' + font;
+		commentCanvas.width = 1;
+		commentCanvas.height = 1;
+		commentCanvasCtx.font = 'bold '+ (cmt.size * devicePixelRatio) + 'px ' + font;
 		commentCanvasCtx.imageSmoothingEnabled = false;
 		cmt.width = ceil(commentCanvasCtx.measureText(cmt.text).width / devicePixelRatio)+2;
 		cmt.height = ceil(cmt.size+3)+2;
@@ -1441,17 +1437,21 @@ var CommentManager = (function() {
 		
 		commentCanvas.width = cmt.oriWidth;
 		commentCanvas.height = cmt.oriHeight;
-		commentCanvasCtx.font = (cmt.size * devicePixelRatio) + 'px ' + font;
-		commentCanvasCtx.lineWidth = round(1 * devicePixelRatio);
+		commentCanvasCtx.font = 'bold '+ (cmt.size * devicePixelRatio) + 'px ' + font;
+		commentCanvasCtx.lineWidth = .7 * devicePixelRatio;
 		commentCanvasCtx.strokeStyle = (cmt._color == 0) ? '#FFFFFF' : '#000000';
 		commentCanvasCtx.textBaseline = 'bottom';
 		commentCanvasCtx.textAlign = 'left';
 		
-		commentCanvasCtx.shadowBlur = round(2 * devicePixelRatio);
-		commentCanvasCtx.shadowColor = (cmt._color == 0) ? '#FFFFFF' : '#000000';
+		if(shadow) {
+			commentCanvasCtx.lineWidth = .25 * devicePixelRatio;
+			commentCanvasCtx.shadowBlur = 2 * devicePixelRatio;
+			commentCanvasCtx.shadowColor = (cmt._color == 0) ? '#FFFFFF' : '#000000';
+		}
 		commentCanvasCtx.fillStyle = '#' + colorGetter(cmt.color);
-		commentCanvasCtx.strokeText(cmt.text, 1, commentCanvas.height-1);
 		commentCanvasCtx.fillText(cmt.text, 1, commentCanvas.height-1);
+		if (outline)
+			commentCanvasCtx.strokeText(cmt.text, 1, commentCanvas.height-1);
 		
 		if(cmt.border){
 			commentCanvasCtx.lineWidth = round(2 * devicePixelRatio);
@@ -1488,12 +1488,12 @@ var CommentManager = (function() {
 	};
 	CommentManager.prototype.sendAsync = function(data){
 		if(data.mode === 8){
-			console.log(data);
 			if(this.scripting){
-				console.log(this.scripting.eval(data.code));
+				this.scripting.eval(data.code);
 			}
 			return;
 		}
+		if(this.options.global.density > 0 && data.mode == 1 && data.border !== true && this.csa.scroll.length>=this.options.global.density) return false;
 		if(this.filter != null){
 			data = this.filter.doModify(data);
 			if(data == null || data === false) return;
@@ -1504,7 +1504,7 @@ var CommentManager = (function() {
 			var now = performance.now();
 			var cmt = new CoreComment(this, data);
 			cmt.dom = {style:{}};
-			commentCanvasDrawer(cmt);
+			commentCanvasDrawer(cmt, this.options.global.outline, this.options.global.shadow);
 			if( data.mode == 1 || data.mode == 6){
 				cmt.rx = 0;
 				cmt.x = this.width;
@@ -1516,8 +1516,8 @@ var CommentManager = (function() {
 			switch(cmt.mode){
 				default:
 				case 1:{this.csa.scroll.add(cmt);}break;
-				case 4:{this.csa.bottom.add(cmt);this.staticUpdate=true;}break;
-				case 5:{this.csa.top.add(cmt);this.staticUpdate=true;}break;
+				case 4:{this.csa.bottom.add(cmt);}break;
+				case 5:{this.csa.top.add(cmt);}break;
 			}
 		}else{
 			
@@ -1574,8 +1574,8 @@ var CommentManager = (function() {
 			default:
 			case 1:{this.csa.scroll.remove(cmt);}break;
 			case 2:{this.csa.scrollbtm.remove(cmt);}break;
-			case 4:{this.csa.bottom.remove(cmt);this.staticUpdate=true;}break;
-			case 5:{this.csa.top.remove(cmt);this.staticUpdate=true;}break;
+			case 4:{this.csa.bottom.remove(cmt);}break;
+			case 5:{this.csa.top.remove(cmt);}break;
 			case 6:{this.csa.reverse.remove(cmt);}break;
 			case 7:break;
 		}
@@ -1749,7 +1749,7 @@ var ABP = {
 	if (!ABP) return;
 	var $$ = jQuery,
 	addEventListener='addEventListener',
-	versionString='HTML5 Player ver.171229 based on ABPlayer-bilibili-ver',
+	versionString='HTML5 Player ver.180320 based on ABPlayer-bilibili-ver',
 	$ = function(e) {
 		return document.getElementById(e);
 	};
@@ -3124,6 +3124,7 @@ var ABP = {
 		bufferColumn.innerHTML=svgStats;
 		bufferColumn=bufferColumn.querySelector('polyline');
 		realtimeBitrateColumn.innerHTML=svgStats;
+		realtimeBitrateColumn.firstChild.lastChild.setAttribute('points', '1,21 180,21 120,22 120,1');
 		realtimeBitrateColumn=realtimeBitrateColumn.querySelector('polyline');
 		downloadSpeedColumn.innerHTML=svgStats;
 		downloadSpeedColumn=downloadSpeedColumn.querySelector('polyline');
@@ -3170,18 +3171,23 @@ var ABP = {
 			if(playerStatsOn){
 				var buffered = video.buffered,
 				clipsContainer=document_querySelector('#buffer-clips>span');
-				
-				var duration = video.duration==Infinity ? buffered.end(buffered.length-1) : video.duration,
-				clipsTitle=[];
-				for(var i=0,start,length,clipsArr=[];i<buffered.length;i++){
-					if(i>=clipsContainer.childNodes.length){
-						clipsContainer.appendChild(_('span',{className:'buffer-clip'}));
+
+				var startOffset = 0,
+        duration = video.duration,
+        clipsTitle = [];
+				if (video.duration == Infinity || video.duration > (buffered.end(buffered.length - 1) + 60) * 1000) {
+					startOffset = buffered.start(0);
+					duration = buffered.end(buffered.length - 1) - startOffset;
+				}
+				for (var i = 0, start, length; i < buffered.length; i++) {
+					if (i >= clipsContainer.childNodes.length) {
+						clipsContainer.appendChild(_('span', { className: 'buffer-clip' }));
 					}
 					start = buffered.start(i);
 					length = buffered.end(i) - start;
-					clipsContainer.childNodes[i].style.left=to2digitFloat(start/duration*100) + '%';
-					clipsContainer.childNodes[i].style.width=to2digitFloat(length / duration * 100) + '%';
-					clipsTitle.push(formatTime(start|0)+' - '+formatTime((start+length)|0));
+					clipsContainer.childNodes[i].style.left = to2digitFloat((start - startOffset) / duration * 100) + '%';
+					clipsContainer.childNodes[i].style.width = to2digitFloat(length / duration * 100) + '%';
+					clipsTitle.push(formatTime(start | 0) + ' - ' + formatTime((start + length) | 0));
 				}
 				while(i<clipsContainer.childNodes.length){
 					clipsContainer.childNodes[i].remove();
@@ -3194,7 +3200,7 @@ var ABP = {
 				flvjsStyle.textContent='';
 				var i=0,mediaInfo=player.flv._mediaInfo,statisticsInfo=player.flv.statisticsInfo,currentTime=video.currentTime,segs=player.flv._mediaDataSource.segments,timeOffset=0,off=0,bitrate,timeIndex;
 				for(timeIndex=0;timeIndex < segs.length;timeIndex++){
-					if(currentTime<=(timeOffset+segs[timeIndex].duration)/1e3){
+					if(currentTime<=(timeOffset+segs[timeIndex].duration||Infinity)/1e3){
 						//console.log(off,timeOffset,currentTime)
 						timeOffset=(currentTime-timeOffset/1e3)|0;
 						break;
@@ -3204,6 +3210,28 @@ var ABP = {
 					}
 				}
 				
+				if(playerStatsOn){
+					var segSeperator = document_querySelector('#buffer-clips>span:nth-of-type(2)'), segSeperatorChild=segSeperator.children;
+					while(segSeperatorChild.length > segs.length){
+						segSeperatorChild[segs.length].remove();
+					}
+					while(segSeperatorChild.length < segs.length){
+						segSeperator.appendChild(_('div',{style:{
+							width:'1px',
+							height:'300%',
+							top:'-100%',
+							position:'absolute',
+							background:'#CCC',
+							left:0
+						}}));
+					}
+					for(var j=1, timeBaseOffset=0; j < segs.length; j++){
+						segSeperatorChild[j].style.left = to2digitFloat((timeBaseOffset+segs[j-1].duration)/1e3 / duration*100) + '%';
+						timeBaseOffset+=segs[j-1].duration
+					}
+				}
+				
+				var segIndex=statisticsInfo.currentSegmentIndex,currentSeg=segs[segIndex]||{},currentSize=currentSeg.filesize|0,currentDuration=currentSeg.duration|0;
 				/*['mimeType','audioCodec','videoCodec'].forEach(function(name){
 					flvjsStats[i++].innerHTML=mediaInfo[name];
 				})*/
@@ -3214,22 +3242,36 @@ var ABP = {
 				flvjsStats[i++].textContent=to2digitFloat(mediaInfo.audioDataRate)+' kbps';
 				
 				if(mediaInfo.bitrateMap){
-					var currentTime=video.currentTime,timeOffset=currentTime|0,off=0,bitrate;
-					if(mediaInfo.bitrateMap[off])
-						bitrate=mediaInfo.bitrateMap[off][timeOffset]
+					
+					var bitrateMap = mediaInfo.bitrateMap;
+					if(bitrateMap[off])
+						bitrate=bitrateMap[off][timeOffset];
 					if(bitrate!=undefined){
-						if(odd && lastCurrent!=(video.currentTime|0)){
-							lastCurrent=video.currentTime|0;
-							flvjsStats[i++].textContent=to2digitFloat(bitrate)+' kbps';
-							realtimeBitrateArr.push(bitrate);
-							realtimeBitrateArr.shift();
-							if(playerStatsOn)
-								renderColumn(realtimeBitrateColumn,realtimeBitrateArr);
-						}else{
-							i++;
-						}
+						flvjsStats[i++].textContent=to2digitFloat(bitrate)+' kbps';
 					}else{
 						flvjsStats[i++].textContent='N/A';
+					}
+					if(playerStatsOn){
+						var realtimeBitrateArr=[];
+						var displaySegOffset = off, time = timeOffset - 40;
+						while (time < 0) {
+							displaySegOffset--;
+							if (!bitrateMap[displaySegOffset]) {
+								displaySegOffset++;
+								break;
+							}
+							time += bitrateMap[displaySegOffset].length;
+						}
+						while (realtimeBitrateArr.length < 60) {
+							if (bitrateMap[displaySegOffset]==undefined) {
+								realtimeBitrateArr.push(0);
+								continue;
+							}
+							realtimeBitrateArr.push(bitrateMap[displaySegOffset][time] ||0);
+							time++;
+							time >= bitrateMap[displaySegOffset].length && (displaySegOffset++, time = 0);
+						}
+						renderColumn(realtimeBitrateColumn,realtimeBitrateArr);
 					}
 				}else{
 					flvjsStats[i++].textContent='N/A'
